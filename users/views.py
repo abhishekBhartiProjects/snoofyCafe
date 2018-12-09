dac27b16ed01cbf6a4c06c1739ae90064baf5cbd
abhishekbharti <abhishek.bharti@testbook.com>
2018-12-09 12:41:07 +0530

    Profile page created

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .form import UserRegisterForm
 
 def register(request):
@@ -16,3 +17,7 @@ def register(request):
 
     print('Invalid form')
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
