2a61a91139e9e2ad6fceb32cbe21dd598cb5c9d5
abhishekbharti <abhishek.bharti@testbook.com>
2018-12-28 01:12:00 +0530

    Blog created and on creation, user is redirected to blog detail page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
 from .models import Post
 
 posts = Post.objects.all()
@@ -18,6 +22,14 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post #If we will stick to the naming convention, we will only require this single line of code.
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
